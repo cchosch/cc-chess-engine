1f6da27a5389c58c4363bad6623ede6607220ae7
cchosch <colinhoscheit@gmail.com>
2022-03-10 14:40:37 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::cmp::Ordering;
+use std::fmt;
 
 enum BoardState{
+    empty,
     king,
     queen,
     rook,
@@ -16,7 +18,60 @@ impl BoardState{
             BoardState::queen => return 9,
             BoardState::rook => return 5,
             BoardState::pawn => return 1,
+            BoardState::empty => return 0,
             _ => return 3,
+        }
+    }
+}
+
+struct Board{
+    board : [BoardState; 64],
+}
+
+impl fmt::Display for BoardState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self{
+            BoardState::king => write!(f, "Kn"),
+            BoardState::queen => write!(f, "Qn"),
+            BoardState::rook => write!(f, "Rk"),
+            BoardState::pawn => write!(f, "Pn"),
+            BoardState::knight => write!(f, "Kn"),
+            BoardState::bishop => write!(f, "Bp"),
+            _ => write!(f, "  ")
+        }
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut c;
+        for s in 0..self.board.len(){
+            c = s % 7;
+            write!(f, "| {} |", self.board[s]);
+            if c == 0 && s / 7 >= 1{
+                writeln!(f, "");
+            }
+        }
+        write!(f, "")
+    }
+}
+
+impl Default for Board {
+    fn default() -> Board {
+        
+    }
+}
+
+impl Board {
+    fn init(&mut self) {
+        let mut r;
+        let mut c;
+        for s in 0..self.board.len(){
+            r = s / 8;
+            c = s % 8;
+            if r == 0 || r == 7{
+                self.board[s] == BoardState::pawn;
+            } 
         }
     }
 }
@@ -46,6 +101,6 @@ impl std::cmp::PartialEq for BoardState{
 }
 
 fn main() {
-    let mut board : [BoardState; 64];
-    println!("Hello, world!");
+    let mut board : Board = Board{..Defualt::default()};
+    println!("{}", board);
 }
